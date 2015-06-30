4b260a9abff72b34d1f72cb65edffeed761e6a27
YumeiLv <562736798@qq.com>
2015-06-30 09:57:49 +0800

    ym

--- Hi鲜果0.0.1/Hi鲜果/项目主框架/AppDelegate.m
+++ Hi鲜果0.0.1/Hi鲜果/项目主框架/AppDelegate.m
@@ -20,6 +20,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    
+    
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     [Framework controllers].window = self.window;
