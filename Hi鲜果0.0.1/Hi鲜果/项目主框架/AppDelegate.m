344e7f692d9716159cc9bafb04a50983660f87e3
李波 <libo@MacBook-Pro.local>
2015-06-30 10:24:37 +0800

    libo

--- Hi鲜果0.0.1/Hi鲜果/项目主框架/AppDelegate.m
+++ Hi鲜果0.0.1/Hi鲜果/项目主框架/AppDelegate.m
@@ -22,7 +22,7 @@
     //ok
     //ok
     //ok
-//JGHJJJH
+//JGHJJJHGHH
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.backgroundColor = [UIColor whiteColor];
     [Framework controllers].window = self.window;
