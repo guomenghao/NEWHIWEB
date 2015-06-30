fbd608c0d690d7779642f637c11df67ce8c682ee
YumeiLv <562736798@qq.com>
2015-06-30 14:06:33 +0800

    1

--- Hi鲜果0.0.1/Hi鲜果/购物车/VIew/CartCell.m
+++ Hi鲜果0.0.1/Hi鲜果/购物车/VIew/CartCell.m
@@ -10,14 +10,15 @@
 
 @implementation CartCell
 
-- (void)awakeFromNib {
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        
+        
+        
+    }
+    return self;
 }
 
 @end
